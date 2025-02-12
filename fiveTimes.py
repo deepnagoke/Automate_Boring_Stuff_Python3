1f7f2788c134407e06f4479b52703e24c4bdbaeb
MANDEEP SHARMA <mandeep.sharma@hotmail.co.uk>
2025-02-12 23:52:48 +0000

    Add files via upload

---
+++ fiveTimes.py
@@ -0,0 +1,27 @@
+#print('My name is:')
+#for i in range(5):
+#    print('Jimmy five times (' + str(i) + ')')
+# The program will print the name five times.
+
+#total = 0
+#for num in range(101):
+#    total = total + num
+#print(total)
+
+# The program will add all the numbers from 0 to 100 and print the total.
+
+#print('My name is')
+#i = 0
+#while i < 5:   
+#    print('Jimmy five times (' + str(i) + ')')
+#    i = i + 1
+
+# The program will print the name five times.
+
+#for i in range(12, 16):
+#    print(i)
+
+# The program will print the numbers from 12 to 15.
+
+#for i in range(0, 10, 2):
+#    print(i)
---
+++ printRandom(CH02).py
@@ -0,0 +1,4 @@
+import random
+for i in range(5):
+    print(random.randint(1, 10))
+# The program will print five random numbers between 1 and 10.
